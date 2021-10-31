4863aee804076a9fd196d9598f0eedc75934cd05
Mehmet Emre Sever <xd_xd_xd_xd-xd@hotmail.com>
2021-10-31 06:14:55 +0300

    11
    11

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def user(name):
 @app.route('/endpoint', methods=['GET', 'POST'])
 def endpoint():
 	print(str(request.get_data()))
-	return request.form['hub.challenge']
+	return request.form['hub_challenge']
 
 if __name__ == '__main__':
     app.run(debug=True)
