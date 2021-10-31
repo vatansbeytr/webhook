67794635531d01ee1c10b968810b5ebffef13595
Mehmet Emre Sever <xd_xd_xd_xd-xd@hotmail.com>
2021-10-31 05:42:20 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+    return "<h1>Hello, World!</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, {0}!</h1>'.format(name)
 
 if __name__ == '__main__':
-
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(debug=True)
